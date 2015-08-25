1b166407be3f06a1421e0534db75aee10631e622
r96942124 <r96942124@users.noreply.github.com>
2015-08-25 18:37:34 +0800

    Update evaluation.cpp

--- expression-evaluation/evaluation.cpp
+++ expression-evaluation/evaluation.cpp
@@ -1,2 +1,32 @@
 #include"evaluation.h"
 
+void Evaluation::calculate()
+{
+  expression.push_back("1");
+  expression.push_back("+");
+  expression.push_back("2");
+  expression.push_back("*");
+  expression.push_back("13");
+  
+  std::cout<<"Please"<<std::endl;
+  getPostfix();
+  doEvaluation();
+}
+
+void Evaluation::getPostfix()
+{
+  ;
+  
+}
+
+void Evaluation::doEvaluation()
+{
+  ;
+}
+
+int main()
+{
+  Evaluation testEval;
+  testEval.calculate();
+  return 0;
+}
