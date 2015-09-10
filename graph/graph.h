cae7f53ac51f50ad9a1dafcafffb29f2b173250e
r96942124 <r96942124@users.noreply.github.com>
2015-09-10 20:39:19 +0800

    Update graph.h

--- graph/graph.h
+++ graph/graph.h
@@ -2,10 +2,10 @@
 
 class graph{
   public:
-    virtual void insertVertx(int vertex) = 0;
-    virtual void insertEdge(int vertexOne, int verTexTwo) = 0;
+    virtual void addVertx(int vertex) = 0;
+    virtual void addEdge(int vertexOne, int verTexTwo) = 0;
     virtual void deleteVertex(int vertex) = 0;
-    virtual void DeleteEdge(int vertexOne, int verTexTwo) = 0;
+    virtual void deleteEdge(int vertexOne, int verTexTwo) = 0;
     virtual boolean isEmpty() = 0;
     virtual void listAdjacent(int vertex) = 0;
 };
