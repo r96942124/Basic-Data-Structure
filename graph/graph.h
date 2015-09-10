ba9a6061314951dfb5d60656e8e6c9aa624d9f7b
r96942124 <r96942124@users.noreply.github.com>
2015-09-10 17:33:21 +0800

    Update graph.h

--- graph/graph.h
+++ graph/graph.h
@@ -1,1 +1,11 @@
 #include<iostream>
+
+class graph{
+  public:
+    void insertVertx(int vertex);
+    void insertEdge(int vertexOne, int verTexTwo);
+    void deleteVertex(int vertex);
+    void DeleteEdge(int vertexOne, int verTexTwo);
+    boolean isEmpty();
+    void listAdjacent(int vertex);
+};
