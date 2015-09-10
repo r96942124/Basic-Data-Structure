300a74c5f2ff0bb637640e15e4165f49fd349aaa
r96942124 <r96942124@users.noreply.github.com>
2015-09-10 20:05:25 +0800

    Create matrixGraph.cpp

---
+++ graph/matrixGraph.cpp
@@ -0,0 +1,12 @@
+#include"matrixGraph.h"
+
+void matrixGraph::insertVertex(int vertex)
+{
+  int index;
+  if (indexMap.end()==indexMap.find(vertex)){
+     if (!indexUnused.empty()){
+        
+     }
+     std::cout<<"Insert Vertex: "<<vertex<<std::endl;
+  }
+}
