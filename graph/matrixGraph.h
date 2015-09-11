5ceefa4ca9aa2c5036cb1225ad0dd00607d3ee7d
r96942124 <r96942124@users.noreply.github.com>
2015-09-11 12:41:44 +0800

    Update matrixGraph.h

--- graph/matrixGraph.h
+++ graph/matrixGraph.h
@@ -1,11 +1,15 @@
 #include"graph.h"
+#include<deque>
 
 class matrixGraph: public graph{
   public:
-      ;
+    void clearEdge(int index);  
   private:
-  std::map<int,int> indexMap;
-  std::stack<int> indexUnused;
-  int indexTop;
-  std::vector<std::vector<int>> matrix;
+    std::string vertexName;
+    std::vector<int> 
+    std::map<char,int> indexMap;
+    std::stack<int> indexUnused;
+    int indexTop;
+    std::vector<std::vector<int>> matrix;
 };
+
