df6965737170e341d5305bd31e1265a5adad9abd
r96942124 <r96942124@users.noreply.github.com>
2015-09-12 11:45:36 +0800

    Update graph.h

--- graph/graph.h
+++ graph/graph.h
@@ -6,8 +6,18 @@ class graph{
     virtual void addEdge(char &vertexOne, char &vertexTwo) = 0;
     virtual void deleteVertex(char &vertex) = 0;
     virtual void deleteEdge(char &vertexOne,char &vertexTwo) = 0;
-    virtual bool isEmpty() = 0;
     virtual bool searchEdge(char &vertexOne,char &vertexTwo) = 0; 
     virtual void listAdjacent(char & vertex) = 0;
+    
+    void addVertx(char index);
+    void addEdge(int indexOne, int indexTwo);
+    void deleteVertex(int index);
+    void deleteEdge(int indexOne,int indexTwo);
+    bool searchEdge(int indexOne,int indexTwo);
+    void listAdjacent(int index);
+    
+    void clearEdge(int index);      
+    virtual bool isEmpty() = 0;
     virtual void printMatrix()=0;
+    
 };
