1972ba02c4afe8a13b86a5ba46e16eadb0eee894
r96942124 <r96942124@users.noreply.github.com>
2015-09-14 15:10:42 +0800

    Update matrixGraph.h

--- graph/matrixGraph.h
+++ graph/matrixGraph.h
@@ -3,14 +3,8 @@
 
 class matrixGraph: public graph{
   public:
-    matrixGraph():vertexName(std::string(10,'\0'),vertexExist(std::vector<int>(matrixSize)),indexTop(0),matrix(std::vector(matrixSize,std::vector(matrixSize))){}
-    void clearEdge(int index);  
-    void addVertx(char index);
-    void addEdge(int indexOne, int indexTwo);
-    void deleteVertex(int index);
-    void deleteEdge(int indexOne,int indexTwo);
-    bool searchEdge(int indexOne,int indexTwo);
-    void listAdjacent(int index);
+    matrixGraph():vertexName(std::string(10,'\0')),vertexExist(std::vector<int>(matrixSize)),indexTop(0),matrix(std::vector(matrixSize,std::vector(matrixSize))){}
+
     
   private:
     const static int matrixSize=10;
