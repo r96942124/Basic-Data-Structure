0685bab679725d9c312d47a3a84465bdd9bf8034
r96942124 <r96942124@users.noreply.github.com>
2015-09-16 21:58:49 +0800

    Create graph.h

---
+++ graph/graph.h
@@ -0,0 +1,26 @@
+#include<iostream>
+
+class graph{
+  public:
+    graph():{}
+    virtual void addVertex(int index) = 0;
+    //virtual char deleteVertex(int index) = 0;
+    
+    void addEdge(int indexOne, int indexTwo){addEdge(indexOne,indexTwo,1);}
+    virtual void addEdge(int indexOne, int indexTwo, int weight) = 0;
+    virtual void deleteEdge(int indexOne,int indexTwo) = 0;
+    
+    int getWeight(int indexOne,int indexTwo);
+    
+    
+    virtual bool searchEdge(int indexOne,int indexTwo) = 0;
+    virtual void listAdjacent(int index) = 0;
+
+    virrual void clearEdge(int index) = 0;
+    
+    virtual bool isEmpty() = 0;
+    virtual void printMatrix()=0;
+    virtual void printMatrix(std::string vertexName)=0;
+ private: 
+    ;
+};
