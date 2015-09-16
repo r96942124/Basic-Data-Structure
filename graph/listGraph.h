229340211a0a135414bb63897166ecc088f59bb9
r96942124 <r96942124@users.noreply.github.com>
2015-09-16 22:27:00 +0800

    Update listGraph.h

--- graph/listGraph.h
+++ graph/listGraph.h
@@ -9,23 +9,24 @@
 class listGraph: public graph{
   public:
     listGraph():{}
-    void addVertex(const char &vertex);
-    void addEdge(const char &vertexOne,const char &vertexTwo);
-    void deleteVertex(const char &vertex);
-    void deleteEdge(const char &vertexOne,const char &vertexTwo);
-    bool searchEdge(const char &vertexOne,const char &vertexTwo);
-    //void listAdjacent(char & vertex);
 
-    //void addVertex(int index);
-    void addEdge(int indexOne, int indexTwo);
-    void deleteVertex(int index);
+    void addVertex(int index);
+    int deleteVertex(int index);
+    
+    void addEdge(int indexOne, int indexTwo,int weight);
     void deleteEdge(int indexOne,int indexTwo);
+    
     bool searchEdge(int indexOne,int indexTwo);
-    //void listAdjacent(int index);
+    void listAdjacent(int index);
 
     void clearEdge(int index);
     bool isEmpty();
     void printMatrix();
+    void printMatrix(std::string vertexName);
   private:
+    typedef struct{
+      int index;
+      Node* nextNode;
+    } Node;
     std::vector<Node*> 
 };
