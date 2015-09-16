43721d6d951c3ec2e8d0d0d78d5bff9974374fc7
r96942124 <r96942124@users.noreply.github.com>
2015-09-17 01:28:18 +0800

    Create multilistGraph.h

---
+++ graph/multilistGraph.h
@@ -0,0 +1,32 @@
+#include"graph.h"
+#include<iostream>
+#include<deque>
+#include<map>
+#include<stack>
+#include<vector>
+#include<iterator>
+
+class multilistGraph: public graph{
+  public:
+    listGraph():{}
+
+    int addVertex(std::string name);
+    //void deleteVertex(int index);
+    
+    void addEdge(int indexOne, int indexTwo,int weight);
+    bool deleteEdge(int indexOne,int indexTwo);
+    
+    bool searchEdge(int indexOne,int indexTwo);
+    void listAdjacent(int index);
+
+    void clearEdge(int index);
+    bool isEmpty();
+    void printMatrix();
+  private:
+    typedef struct{
+      int index;
+      int weight;
+      Node* nextNode;
+    } Node;
+    std::vector<Node*> nodeList;
+};
